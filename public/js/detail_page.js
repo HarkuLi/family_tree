3f21d3305cb5f082720e8c9c9cc373dcc8f193aa
nick.li <nick.li@nicklide-MacBook-Pro.local>
2017-07-31 17:43:52 +0800

    new func: edit and delete dialog of a person

--- public/js/detail_page.js
+++ public/js/detail_page.js
@@ -1,7 +1,13 @@
-$(()=>{ //ready
-  var detail;
-  var id;
+var dialog_edit_count = 0;
+var detail;
+var id;
+var old_pat, old_res;
 
+/**
+ * ready
+ */
+
+$(()=>{
   //record details
   detail = {};
   for(let ele of $("#node_detail > p")){
@@ -24,85 +30,188 @@ $(()=>{ //ready
       endEdit();
   });
 
-  var convertToEditMod = ()=>{
-    $("#edit_detail").text("save");
-
-    //remove contents
-    $("#node_detail > p").remove();
-    
-    //create inputs according to details
-    for(let prop in detail){
-      let title = $("<strong></strong>");
-      let input = $("<input></input>");
-      $(title).text(prop+": ");
-      $(input).attr("type", "text");
-      $(input).attr("name", prop);
-      $(input).attr("value", detail[prop]);
-      $("#node_detail").append(title);
-      $("#node_detail").append(input);
-      $("#node_detail").append("<br>");
+  //couldn't use arrow function here, or "this" will point to window object
+  //because "this" is fixed when arrow function declared
+  $("#dialog_list > .btn-primary").click(function(){
+    var self = this;
+    if($(self).text() === "edit"){
+      if(dialog_edit_count)  return alert("請先結束進行中的編輯");
+      dialogEdit(self);
     }
-  };
-
-  var endEdit = ()=>{
-    var prop = [];
-    var value = [];
-    var modified = false;
-    var udata = {};
-
-    $("#edit_detail").text("edit");
-    if(!detail) return;
-
-    //record inputs
-    for(let ele of $("#node_detail > strong")){
-      let new_prop = $(ele).text();
-      new_prop = new_prop.substr(0, new_prop.length-2);
-      prop.push(new_prop);
+    else if($(self).text() === "save"){
+      if(dialog_edit_count !== 1) return;
+      dialogSave(self);
     }
-    for(let ele of $("#node_detail > :text"))
-      value.push($(ele).val());
+  });
 
-    //compare with original data
-    for(let i=0; i<prop.length; ++i){
-      if(value[i] !== detail[prop[i]]){
-        detail[prop[i]] = value[i];
-        udata[prop[i]] = value[i];
-        modified = true;
-      }
-    }
-
-    //update database when modified
-    new Promise((resolve)=>{
-      if(!modified) resolve(false);
-      else  resolve(updateData(id, udata));
-    })
-    .then(()=>{
-      //remove inputs
-      $("#node_detail > strong").remove();
-      $("#node_detail > :text").remove();
-      $("#node_detail > br").remove();
-      
-      //create contents according to details
-      $("#node_detail").append("<br>");
-      for(let prop in detail){
-        let p = $("<p></p>");
-        let strong = $("<strong></strong>");
-        $(strong).text(prop+": ");
-        $(p).append(strong);
-        $(p).append(detail[prop]);
-        $("#node_detail").append(p);
-      }
-    });
-  };
-
-  /** return: {update_count} */
-  var updateData = (id, update_data)=>{
-    update_data._id = id;
-    return new Promise((resolve, reject)=>{
-      $.post("/tree/update_person", update_data, (data, status)=>{
-        if(status !== "success")  return reject("post status: "+status);
-        resolve(data);
-      });
-    });
-  };
+  //couldn't use arrow function here, or "this" will point to window object
+  //because "this" is fixed when arrow function declared
+  $("#dialog_list > .btn-danger").click(function(){
+    var self = this;
+    if(dialog_edit_count) return alert("請先結束進行中的編輯");
+    dialogDel(self);
+  });
 });
+
+/**
+ * functions
+ */
+
+var convertToEditMod = ()=>{
+  $("#edit_detail").text("save");
+
+  //remove contents
+  $("#node_detail > p").remove();
+  
+  //create inputs according to details
+  for(let prop in detail){
+    let title = $("<strong></strong>");
+    let input = $("<input></input>");
+    $(title).text(prop+": ");
+    $(input).attr("type", "text");
+    $(input).attr("name", prop);
+    $(input).attr("value", detail[prop]);
+    $("#node_detail").append(title);
+    $("#node_detail").append(input);
+    $("#node_detail").append("<br>");
+  }
+};
+
+var endEdit = ()=>{
+  var prop = [];
+  var value = [];
+  var modified = false;
+  var udata = {};
+
+  $("#edit_detail").text("edit");
+  if(!detail) return;
+
+  //record inputs
+  for(let ele of $("#node_detail > strong")){
+    let new_prop = $(ele).text();
+    new_prop = new_prop.substr(0, new_prop.length-2);
+    prop.push(new_prop);
+  }
+  for(let ele of $("#node_detail > :text"))
+    value.push($(ele).val());
+
+  //compare with original data
+  for(let i=0; i<prop.length; ++i){
+    if(value[i] !== detail[prop[i]]){
+      detail[prop[i]] = value[i];
+      udata[prop[i]] = value[i];
+      modified = true;
+    }
+  }
+
+  //update database when modified
+  new Promise((resolve)=>{
+    if(!modified) resolve(false);
+    else  resolve(updatePerson(udata));
+  })
+  .then(()=>{
+    //remove inputs
+    $("#node_detail > strong").remove();
+    $("#node_detail > :text").remove();
+    $("#node_detail > br").remove();
+    
+    //create contents according to details
+    $("#node_detail").append("<br>");
+    for(let prop in detail){
+      let p = $("<p></p>");
+      let strong = $("<strong></strong>");
+      $(strong).text(prop+": ");
+      $(p).append(strong);
+      $(p).append(detail[prop]);
+      $("#node_detail").append(p);
+    }
+  });
+};
+
+/** return: {update_count} */
+var updatePerson = (update_data)=>{
+  update_data._id = id;
+  return new Promise((resolve, reject)=>{
+    $.post("/tree/update_person", update_data, (data, status)=>{
+      if(status !== "success")  return reject("post status: "+status);
+      resolve(data);
+    });
+  });
+};
+
+var dialogEdit = (self) => {
+  var res = $(self).prev();
+  var pat = $(res).prev().prev();
+
+  old_pat = $(pat).val();
+  old_res = $(res).val();
+
+  $(res).prop("disabled", false);
+  $(self).text("save");
+  ++dialog_edit_count;
+};
+
+var dialogSave = (self) => {
+  var res = $(self).prev();
+  var pat = $(res).prev().prev();
+
+  new Promise(resolve => {
+    if($(pat).val() !== old_pat) resolve(false);
+    else if($(res).val() === old_res) resolve(false);
+    else resolve(updateDialog($(res).val()));
+  })
+  .then(() => {
+    $(res).prop("disabled", true);
+    $(self).text("edit");
+    --dialog_edit_count;
+  });
+};
+
+var dialogDel = (self) => {
+  if(!confirm("確定要刪除嗎?")) return;
+  var res = $(self).prev().prev();
+  var pat = $(res).prev().prev();
+
+  $(self).prevUntil("br").remove();
+  $(self).next().remove();
+  $(self).remove();
+  
+  deleteDialog($(pat).val(), $(res).val())
+    .then(() => {
+      //remove elements of the row
+      
+    });
+};
+
+var updateDialog = (new_res) => {
+  console.log("update dialog");
+  var passed_data = {
+    talkerId: id,
+    old_pat,
+    old_res,
+    new_res
+  };
+
+  return new Promise((resolve, reject)=>{
+    $.post("/tree/update_dialog", passed_data, (data, status)=>{
+      if(status !== "success")  return reject("post status: "+status);
+      resolve(data);
+    });
+  });
+};
+
+var deleteDialog = (pat, res) => {
+  console.log("delete dialog");
+  var passed_data = {
+    talkerId: id,
+    pat,
+    res
+  };
+
+  return new Promise((resolve, reject)=>{
+    $.post("/tree/delete_dialog", passed_data, (data, status)=>{
+      if(status !== "success")  return reject("post status: "+status);
+      resolve(data);
+    });
+  });
+}
